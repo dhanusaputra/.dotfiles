eff880521ef90e52e01188ac68d4df4d3e281579
dhanu <andreasdhanu@gmail.com>
2020-11-19 20:50:12 +0700

    fix

--- init.sh
+++ init.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo "Configuring ..."
 stow -vt ~ tmux vim
-echo "Installing vundle ..."
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2> /dev/null
+if [ ! -d ~/.vim/bundle/Vundle.vim ]
+then
+  echo "Installing vundle ..."
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 echo "Setup vim ..."
 vim +'PluginInstall' +'qa!'
 echo "Done!"

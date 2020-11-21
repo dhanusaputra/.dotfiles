ec23021db2bd4a45b5c0054c8cc31a385e3bcfa6
dhanu <andreasdhanu@gmail.com>
2020-11-21 18:28:25 +0700

    add git

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-packages="tmux vim"
+packages="tmux vim git"
 
 if [ ! -z "$@"  ]
 then
@@ -11,6 +11,7 @@ fi
 
 echo "Configuring $packages..."
 stow -vt ~ $packages
+
 if [[ $packages == *"vim"*  ]]
   then
   if [ ! -d ~/.vim/bundle/Vundle.vim ]
@@ -21,5 +22,12 @@ if [[ $packages == *"vim"*  ]]
   echo "Setup vim..."
   vim +'PluginInstall' +'qa!'
 fi
+
+if [[ $packages == *"git"*  ]]
+then
+  echo "Setup git..."
+  git config --global core.excludesfile ~/.gitignore_global
+fi
+
 echo "Done!"
 

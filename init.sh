a7a467674194185a38a6acda477747bce179d1a6
dhanu <andreasdhanu@gmail.com>
2020-11-21 18:10:40 +0700

    enable options

--- init.sh
+++ init.sh
@@ -2,14 +2,24 @@
 
 set -e
 
-echo "Configuring ..."
-stow -vt ~ tmux vim
-if [ ! -d ~/.vim/bundle/Vundle.vim ]
+packages="tmux vim"
+
+if [ ! -z "$@"  ]
 then
-  echo "Installing vundle ..."
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  packages=$@
 fi
-echo "Setup vim ..."
-vim +'PluginInstall' +'qa!'
+
+echo "Configuring $packages..."
+stow -vt ~ $packages
+if [[ $packages == *"vim"*  ]]
+  then
+  if [ ! -d ~/.vim/bundle/Vundle.vim ]
+  then
+    echo "Installing vundle..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  fi
+  echo "Setup vim..."
+  vim +'PluginInstall' +'qa!'
+fi
 echo "Done!"
 
